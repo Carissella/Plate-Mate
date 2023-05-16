90910e3670291534f6bf8c48c8374ccb553ccce6
Carisse Barr <carissebarr.swe@gmail.com>
2023-05-15 20:45:26 -0400

    updated react app and added server folder and files

---
+++ my-app/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+  });
