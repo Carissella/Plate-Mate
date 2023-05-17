235ebef94468ab97a3df73fc860f44c7e28a4bec
Carisse Barr <carissebarr.swe@gmail.com>
2023-05-16 20:41:57 -0400

    added models, typedefs, resolvers, seed.js, and utils

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,48 @@
 const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    recipes: [Recipe]!
+  }
+
+  type Recipe {
+    _id: ID
+    recipeText: String
+    recipeAuthor: String
+    createdAt: String
+    comments: [Comment]!
+  }
+
+  type Comment {
+    _id: ID
+    commentText: String
+    commentAuthor: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    recipes(username: String): [Recipe]
+    recipe(recipeId: ID!): Recipe
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addRecipe(recipeText: String!): Recipe
+    removeRecipe(recipeId: ID!): Recipe
+  }
+`;
+
+module.exports = typeDefs;
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,1 +1,30 @@
 const db = require('../config/connection');
+const { User, Recipe } = require('../models');
+
+db.once('open', async () => {
+    try {
+      await Recipe.deleteMany({});
+      await User.deleteMany({});
+  
+      await User.create(userSeeds);
+  
+      for (let i = 0; i < recipeSeeds.length; i++) {
+        const { _id, recipeAuthor } = await Recipe.create(recipeSeeds[i]);
+        const user = await User.findOneAndUpdate(
+          { username: recipeAuthor },
+          {
+            $addToSet: {
+              recipes: _id,
+            },
+          }
+        );
+      }
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  
+    console.log('all done!');
+    process.exit(0);
+  });
+  
